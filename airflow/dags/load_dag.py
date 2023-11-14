37346d493df1826bde622bd30646c5a6ff2d6d10
Tornike Giorgaia <86513043+T0rnik3@users.noreply.github.com>
2023-11-14 16:15:58 +0000

    Load Finished

--- airflow/dags/load_dag.py
+++ airflow/dags/load_dag.py
@@ -1,11 +1,12 @@
 '''Load DAG'''
 from datetime import datetime
-from airflow.operators.bash import BashOperator
+
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 
 with DAG(
     'load_dag',
-    start_date=datetime(2023,1,1),
+    start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False
 ) as dag:
